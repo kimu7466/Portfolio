c6731944c9f3c6d697bd2f1e59693b0960efa9f0
kimu7466 <kimu7466@gmail.com>
2024-09-14 23:35:42 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
